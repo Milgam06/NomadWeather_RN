5f06704ddeebbb7630fee5c1e2cd2e3fe25604ba
Milgam06 <milgamfruit@gmail.com>
2024-05-27 23:58:11 +0900

    start to make Weather App

--- App.js
+++ App.js
@@ -3,22 +3,53 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello Milgam! Welcome to RN World!</Text>
-      <StatusBar
-        style="auto
-      "
-      />
-    </View>
+    <>
+      <StatusBar style="black" />
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <View style={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>23</Text>
+            <Text style={styles.description}>Clouds</Text>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#FFD700",
+  },
+  city: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+
+  cityName: {
+    color: "black",
+    fontSize: 70,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 2.2,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 170,
+    fontWeight: "700",
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 50,
   },
 });
